c041714193f779c4645062fd5fcbc548c445ea42
adelutfi <adelutfinofitasari6@gmail.com>
2018-06-29 11:22:44 +0700

    praktek4

---
+++ array-modif.php
@@ -0,0 +1,14 @@
+<?php
+$data = array(
+     "nama" => "adelutfi",
+	 "usia" => 20,
+	 "lajang" => true,
+	 0.1 =>5
+);
+
+echo "NAMA : " .$data["nama"] . "<br>";
+usia "USIA : " .$data["usia"] . "<br>";
+echo "STATUS LAJANG " " . $data["lajang"] . "<br>
+echo "data lain : " .$data["lajang"] . "<br>
+echo "data lain : " .$data[0.1];
+?>
