bdd123592f3ebad0e1669510bd5dab9f2c1b85cf
adelutfi <36694852+adelutfi@users.noreply.github.com>
2018-06-30 12:10:46 +0700

    Update array-modif.php

--- array-modif.php
+++ array-modif.php
@@ -1,14 +1,12 @@
 <?php
 $data = array(
-     "nama" => "adelutfi",
-	 "usia" => 20,
-	 "lajang" => true,
-	 0.1 =>5
+	"nama" => "Ade lutfi",
+	"usia" => 19,
+	"jomblo" => true,
+	0.1 => 5
 );
-
-echo "NAMA : " .$data["nama"] . "<br>";
-usia "USIA : " .$data["usia"] . "<br>";
-echo "STATUS LAJANG " " . $data["lajang"] . "<br>
-echo "data lain : " .$data["lajang"] . "<br>
-echo "data lain : " .$data[0.1];
+echo "NAMA : " . $data["nama"]. "<br>";
+echo "USIA : " . $data["usia"]. "<br>";
+echo "STATUS JOMBLO : ". $data["jomblo"]. "<br>";
+echo "data lain : ". $data[0.1];
 ?>
